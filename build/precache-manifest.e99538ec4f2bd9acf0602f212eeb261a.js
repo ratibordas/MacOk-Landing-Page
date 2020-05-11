a3f01e8165f3d40b20086586f2f76731f3ec60b5
owner <owner@owners-MacBook-Pro.local>
2020-05-11 15:04:27 +0300

    run build

---
+++ build/precache-manifest.e99538ec4f2bd9acf0602f212eeb261a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c30a5e3e02d0166bdb94908c8a53b093",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae1632a4d7e8332a2752",
+    "url": "/static/css/2.c60feeff.chunk.css"
+  },
+  {
+    "revision": "c25a1f47faf2d0f5ce8c",
+    "url": "/static/css/main.94205c69.chunk.css"
+  },
+  {
+    "revision": "ae1632a4d7e8332a2752",
+    "url": "/static/js/2.f32e58bd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f32e58bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c25a1f47faf2d0f5ce8c",
+    "url": "/static/js/main.dfb9a02f.chunk.js"
+  },
+  {
+    "revision": "00bc482c2a042829b9ea",
+    "url": "/static/js/runtime-main.1f29671d.js"
+  },
+  {
+    "revision": "15661bd5474d8ca0d24677d6458fbdc0",
+    "url": "/static/media/el-alce-web-njX7_Ixraio-unsplash.15661bd5.jpg"
+  },
+  {
+    "revision": "a5b3083cc18386fc75abb8b4ce0e1ff4",
+    "url": "/static/media/instagram-brands.a5b3083c.svg"
+  },
+  {
+    "revision": "9e2a90321a1b0950b2de78562bee98d3",
+    "url": "/static/media/macbook-mockup-png.9e2a9032.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "c75b39fec5f18f8874f2ba9222f2a132",
+    "url": "/static/media/telegram-brands.c75b39fe.svg"
+  },
+  {
+    "revision": "040b385cd1b0ed2402e8042687732f47",
+    "url": "/static/media/viber-brands.040b385c.svg"
+  },
+  {
+    "revision": "f6b857f30f521cf8b8d6034e7068d8ca",
+    "url": "/static/media/whatsapp-brands.f6b857f3.svg"
+  }
+]);
