4c6f6545c452136c7bc95e4aae0c704818b2ddca
owner <owner@owners-MacBook-Pro.local>
2020-05-11 15:29:57 +0300

    Updates

---
+++ precache-manifest.9905e4f906f8e0bae87f224e44fcd871.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ac17687f09ce56b04724cf871ed8d9",
+    "url": "/MacOk-Landing-Page/index.html"
+  },
+  {
+    "revision": "5e67e5d37566eefa43e0",
+    "url": "/MacOk-Landing-Page/static/css/2.21a324d2.chunk.css"
+  },
+  {
+    "revision": "0217a971d34c59f4c1c9",
+    "url": "/MacOk-Landing-Page/static/css/main.a794ea01.chunk.css"
+  },
+  {
+    "revision": "5e67e5d37566eefa43e0",
+    "url": "/MacOk-Landing-Page/static/js/2.32a58a3d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MacOk-Landing-Page/static/js/2.32a58a3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0217a971d34c59f4c1c9",
+    "url": "/MacOk-Landing-Page/static/js/main.454bba33.chunk.js"
+  },
+  {
+    "revision": "d8fa69654b9f47ef00d6",
+    "url": "/MacOk-Landing-Page/static/js/runtime-main.8f8864cc.js"
+  },
+  {
+    "revision": "15661bd5474d8ca0d24677d6458fbdc0",
+    "url": "/MacOk-Landing-Page/static/media/el-alce-web-njX7_Ixraio-unsplash.15661bd5.jpg"
+  },
+  {
+    "revision": "a5b3083cc18386fc75abb8b4ce0e1ff4",
+    "url": "/MacOk-Landing-Page/static/media/instagram-brands.a5b3083c.svg"
+  },
+  {
+    "revision": "9e2a90321a1b0950b2de78562bee98d3",
+    "url": "/MacOk-Landing-Page/static/media/macbook-mockup-png.9e2a9032.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/MacOk-Landing-Page/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/MacOk-Landing-Page/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/MacOk-Landing-Page/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/MacOk-Landing-Page/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "c75b39fec5f18f8874f2ba9222f2a132",
+    "url": "/MacOk-Landing-Page/static/media/telegram-brands.c75b39fe.svg"
+  },
+  {
+    "revision": "040b385cd1b0ed2402e8042687732f47",
+    "url": "/MacOk-Landing-Page/static/media/viber-brands.040b385c.svg"
+  },
+  {
+    "revision": "f6b857f30f521cf8b8d6034e7068d8ca",
+    "url": "/MacOk-Landing-Page/static/media/whatsapp-brands.f6b857f3.svg"
+  }
+]);
